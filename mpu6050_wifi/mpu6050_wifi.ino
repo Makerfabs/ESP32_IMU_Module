6662e71abc1592eebbad0bb4878e49e0c844881f
195cn <2259761058@qq.com>
2020-07-22 11:59:57 +0800

    create project

---
+++ mpu6050_wifi/mpu6050_wifi.ino
@@ -0,0 +1,189 @@
+#define MAKEPYTHON_ESP32_SDA 4
+#define MAKEPYTHON_ESP32_SCL 5
+
+#include <WiFi.h>
+
+const char *ssid = "Makerfabs";
+const char *password = "20160704";
+
+const char *host = "192.168.1.117"; //欲访问的域名
+WiFiClient client;
+
+////////////////////////////////////
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#define INTERRUPT_PIN 35
+#define LED_PIN 13
+bool blinkState = false;
+
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
+Quaternion q;
+VectorInt16 aa;
+VectorInt16 aaReal;
+VectorInt16 aaWorld;
+VectorFloat gravity;
+float euler[3];
+float ypr[3];
+uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
+
+volatile bool mpuInterrupt = false;
+void dmpDataReady()
+{
+  mpuInterrupt = true;
+}
+
+void setup()
+{
+  pinMode(21, OUTPUT);
+  digitalWrite(21, LOW);
+  Wire.begin(MAKEPYTHON_ESP32_SDA, MAKEPYTHON_ESP32_SCL);
+  Wire.setClock(400000);
+
+  Serial.begin(115200);
+  Serial.println("Hello World.....");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.setSleep(false); //关闭STA模式下wifi休眠，提高响应速度
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected");
+  Serial.print("IP Address:");
+  Serial.println(WiFi.localIP());
+
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+/*
+  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  while (Serial.available() && Serial.read())
+    ; // empty buffer
+  while (!Serial.available())
+    ; // wait for data
+  while (Serial.available() && Serial.read())
+    ; // empty buffer again
+*/
+
+  if (client.connect(host, 80)) //80为一般网站的端口号
+  {
+    Serial.println("访问成功");
+    client.print(String("GET /") + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" +
+                 "\r\n");
+  }
+
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1688);
+  if (devStatus == 0)
+  {
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  else
+  {
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+
+  if (!dmpReady)
+    return;
+
+  while (!mpuInterrupt && fifoCount < packetSize)
+  {
+    if (mpuInterrupt && fifoCount < packetSize)
+    {
+      fifoCount = mpu.getFIFOCount();
+    }
+  }
+
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  fifoCount = mpu.getFIFOCount();
+
+  if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024)
+  {
+    mpu.resetFIFO();
+    fifoCount = mpu.getFIFOCount();
+    Serial.println(F("FIFO overflow!"));
+  }
+  else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT))
+  {
+    while (fifoCount < packetSize)
+      fifoCount = mpu.getFIFOCount();
+
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    fifoCount -= packetSize;
+
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[2] * 180 / M_PI);
+    Serial.print("\t");
+
+    Serial.println("#");
+    if (client.connected())
+    {
+      client.print("y");
+      client.print(ypr[0] * 180 / M_PI);
+      client.print("yp");
+      client.print(ypr[1] * 180 / M_PI);
+      client.print("pr");
+      client.print(ypr[2] * 180 / M_PI);
+      client.println("r#");
+    }
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+  }
+}
---
+++ readme.md
@@ -0,0 +1,99 @@
+# ESP32_IMU_Module
+
+```
+/*
+Version:		V1.0
+Author:			Vincent
+Create Date:	2020/7/22
+Note：
+*/
+```
+
+![main](md_pic/main.png)
+
+*The following two project were briefly modified.*
+
+[PyTeapot-Quaternion-Euler-cube-rotation](https://github.com/thecountoftuscany/PyTeapot-Quaternion-Euler-cube-rotation)
+
+[i2cdevlib](https://github.com/jrowberg/i2cdevlib)
+
+[YouTube:6 Axis IMU Sensor with ESP32](https://www.youtube.com/watch?v=PJSM8G_pLW8&feature=youtu.be)
+
+[toc]
+
+# Overview
+
+[Makerfabs home page](https://www.makerfabs.com/)
+
+[Makerfabs Wiki](https://makerfabs.com/wiki/index.php?title=Main_Page)
+
+At present, it is only a technical verification of IMU, and there may be a real project in the future. The hardware is in develop.
+
+Through the socket to achieve wifi transmission euler Angle, and display on the PC side of the Python program.To facilitate testing, I packaged a Win10 64-bit EXE program that can be accessed by changing the IP in the ESP32 program.Otherwise, you need to install a dependent library like OpenGL.
+
+
+
+# ESP32 IMU Module V1.1
+
+The Makerfabs IMU Module features the 6-axis MPU-6050 MEMS sensor from InvenSense. Each of these 6DoF IMU feature an ESP32 with a MPU-6050 which contains a 3-axis gyroscope as well as a 3-axis accelerometer. The MPU-6050 uses 16-bit analog-to-digital converters (ADCs) for digitizing 6 axes. By combining a MEMS 3-axis gyroscope and a 3-axis accelerometer on the same silicon die together with an onboard Digital Motion Processor™ (DMP™) .It can be used as a helicopter/quadcopter.
+
+## Feature
+
+- Integrated ESP32 2.4G WiFi and Bluetooth
+- I2C Digital-output of 6-axis MotionFusion data in rotation matrix, quaternion, Euler Angle, or raw data format
+- Input Voltage: 3.7V Battery or 5.0V MicroUSB 
+- Tri-Axis angular rate sensor (gyro) with a sensitivity up to 131 LSBs/dps and a full-scale range of ±250, ±500, ±1000, and ±2000dps
+- Tri-Axis accelerometer with a programmable full scale range of ±2g, ±4g, ±8g and ±16g
+- Digital Motion Processing™ (DMP™) engine offloads complex MotionFusion, sensor timing synchronization and gesture detection
+- Embedded algorithms for run-time bias and compass calibration. No user intervention required
+- Digital-output temperature sensor
+- Size: max 100mmX100mmX100mm
+
+![front](md_pic/front.jpg)
+
+![back](md_pic/back.jpg)
+
+
+
+# Steps
+
+## PC
+
+- If you're on Windows 64-bit, you can run "/ESP32_IMU_Module/imu_show/dist/imu_show.exe".
+- Other platform can run "/ESP32_IMU_Module/imu_show/imu_show.py".(Dependency libraries such as OpenGL need to be installed manually)
+- After run program,wait seconds.
+- A line of native IP is displayed.
+- Leave the Python program window still and prepare ESP32.
+
+## ESP32
+
+- Open "/ESP32_IMU_Module/mpu6050_wifi/mpu6050_wifi.ino"
+- Change IP ,your Wifi ssid and password.
+
+```c++
+const char *ssid = "Makerfabs";
+const char *password = "20160704";
+const char *host = "192.168.1.117"; 
+```
+
+- Select Tools/Board/BoardsManager.
+- Search esp32.
+- Install esp32 by Espressif Systems.
+
+![install_esp32](md_pic/install_esp32.png)
+
+- Select Toos/board/Esp32 Dev Modue.
+
+![select_board](md_pic/select_board.png)
+
+- Connect ESP32 to your PC via Micro USB .
+- Select Port.
+- And at last ,push upload.
+
+## Show On PC
+
+- Once ESP32 is ready, it usually restarts automatically, and the Python window displays the mock up image.
+- If there is no restart, you need to press RST to reboot manually.
+
+![pc_show](md_pic/pc_show.png)
+
